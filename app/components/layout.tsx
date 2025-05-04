5041659aaf3871453d256d87decd93a8ac6c2e8a
Karungg <miftahfadilah71@gmail.com>
2025-05-04 19:33:11 +0700

    modify layout

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="flex w-full max-w-7xl justify-between gap-4 p-4">
-        <div className="w-1/2 p-4 rounded-lg">
+        <div className="w-1/2 flex items-center p-4 rounded-lg">
           <h1 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
             To do list
           </h1>
