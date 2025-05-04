0788d105a4d31b01a38662f947f29a4d70ad7c0a
Karungg <miftahfadilah71@gmail.com>
2025-05-04 15:05:32 +0700

    add layout component: create reusable component

---
+++ app/components/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="flex w-full max-w-7xl justify-between gap-4 p-4">
+        <div className="w-1/2 p-4 rounded-lg">
+          <h1 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
+            To do list
+          </h1>
+        </div>
+        <div className="w-1/2 p-4 rounded-lg">
+          <h1 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
+            {children}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
